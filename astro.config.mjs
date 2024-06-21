864d921dee25a9aed1e4f890a1661d18647790ff
Alejandro Fernández <108556384+AlejFernandezV@users.noreply.github.com>
2024-06-20 21:41:34 -0500

    Update changes for ing-fernandez.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react';
+import robotsTxt from "astro-robots-txt";
 
-import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
-})
+  integrations: [tailwind(), robotsTxt(), react({
+    experimentalReactChildren: true,
+    include: ['./src/components/magicui/*']
+  })],
+  site: 'https://ing-fernandez.com/'
+});
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      animation: {
+        "border-beam": "border-beam calc(var(--duration)*1s) infinite linear",
+        meteor: "meteor 5s linear infinite",
+        backgroundPositionSpin:
+          "background-position-spin 3000ms infinite alternate",
+        grid: "grid 15s linear infinite",
+      },
+      keyframes: {
+        "border-beam": {
+          "100%": {
+            "offset-distance": "100%",
+          },
+        },
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: 1 },
+          "70%": { opacity: 1 },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: 0,
+          },
+        },
+        "background-position-spin": {
+          "0%": { backgroundPosition: "top center" },
+          "100%": { backgroundPosition: "bottom center" },
+        },
+        grid: {
+          "0%": { transform: "translateY(-50%)" },
+          "100%": { transform: "translateY(0)" },
+        },
+      },
+      colors: {
+        "dark-blue": "#00008B", // Azul oscuro para el modo claro
+        aquamarine: "#7FFFD4", // Azul aguamarina para el modo oscuro
+        turquoise: "#5dc1b9",
+        "far-blue": "#49678d",
+      },
+    },
+  },
+  plugins: [],
+};
